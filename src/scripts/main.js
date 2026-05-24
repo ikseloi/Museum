dd01ec46d7ec5e0d00c97ffa58839f381498cb98
ikseloi <gdalexgo@gmail.com>
2026-05-24 23:48:21 +0300

    add solution

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,1 +1,21 @@
 'use strict';
+
+const track = document.querySelector('.slider__track');
+const sliderItem = document.querySelector('.slider__item');
+const dots = document.querySelectorAll('.slider__dot');
+
+// const slideWidth = track.offsetWidth + 20;
+// const slideWidth = sliderItem.offsetWidth + 20;
+const slideWidth = sliderItem.offsetWidth === 280 ? 300 : 280;
+
+dots.forEach((dot, index) => {
+  dot.addEventListener('click', () => {
+    track.style.transform = `translateX(-${index * slideWidth}px)`;
+
+    dots.forEach((item) => {
+      item.classList.remove('slider__dot--active');
+    });
+
+    dot.classList.add('slider__dot--active');
+  });
+});
